acbc612818b4f65ce7fe3f63958c5c5941099d51
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2023-09-16 11:11:54 -0300

    feat: animation displays coordinates

--- ex1/animation.py
+++ ex1/animation.py
@@ -1,51 +1,63 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
-from PIL import Image
-
-# Define the weights
-weights = [
-    [0.6169326813135259, -0.6273719977579983, 0.9660360352747861],
-    [0.6169326813135259, -0.6273719977579983, 0.9660360352747861],
-    [0.21693268131352583, -0.22737199775799832, 0.5660360352747861],
-    [-0.1830673186864742, 0.1726280022420017, 0.16603603527478605]
-]
-
-# Create a figure and axis
-fig, ax = plt.subplots()
-
-# Set the axis limits
-ax.set_xlim(-2, 2)
-ax.set_ylim(-2, 2)
-
-# Create a line object
-line, = ax.plot([], [], label='Line', color='blue')
+import os
+import json
 
 
-# Function to initialize the plot
-def init():
-    line.set_data([], [])
-    return line,
+def animate_lines(input_data, file_name=None):
+    if file_name is None:
+        path = "../results"
+        files = os.listdir(path)
+        files.sort()
+        file_name = f"{path}/{files[-1]}"
 
+    with open(file_name) as file:
+        results = json.load(file)
 
-# Function to update the plot
-def update(frame):
-    ax.clear()
+    # Create a figure and axis
+    fig, ax = plt.subplots()
+
+    # Set the axis limits
     ax.set_xlim(-2, 2)
     ax.set_ylim(-2, 2)
 
-    w0, w1, w2 = weights[frame]
-    x = np.linspace(-2, 2, 100)
-    y = (-w0 - w1 * x) / w2
-    ax.plot(x, y, label=f'W{frame} Line', color='blue')
-    ax.legend()
+    # Create a line object
+    line, = ax.plot([], [], label='Line', color='blue')
+
+    # Create a scatter plot for input coordinates
+    input_data = np.array(input_data)
+    scatter = ax.scatter([], [], c='red', marker='o', label='Input Coordinates')
+
+    # Function to initialize the plot
+    def init():
+        line.set_data([], [])
+        return line,
+
+    # Function to update the plot
+    def update(frame):
+        ax.set_xlim(-2, 2)
+        ax.set_ylim(-2, 2)
+
+        w0, w1, w2 = results['weights'][frame]
+        x = np.linspace(-2, 2, 100)
+        y = (-w0 - w1 * x) / w2
+        line.set_data(x, y)  # Update the line
+
+        # Update the input coordinates
+        scatter.set_offsets(input_data)
+
+        ax.legend()
+
+    # Create the animation
+    ani = FuncAnimation(fig, update, frames=len(results['weights']), init_func=init, interval=1000)
+
+    # Save the animation as a GIF
+    ani.save('line_animation.gif', writer='pillow', fps=1)
+
+    # Display the animation (optional)
+    plt.show()
 
 
-# Create the animation
-ani = FuncAnimation(fig, update, frames=len(weights), interval=1000)
-
-# Save the animation as a GIF
-ani.save('line_animation.gif', writer='pillow', fps=1)
-
-# Display the animation (optional)
-plt.show()
+input_data2 = [[-1, 1], [1, -1], [1, 1], [-1, -1]]
+animate_lines(input_data2)
--- ex1/main.py
+++ ex1/main.py
@@ -1,5 +1,6 @@
 from perceptron import perceptron
 from simple_perceptron import *
+from animation import animate_lines
 
 
 
@@ -10,9 +11,14 @@ def main():
     input_data_xor = [[-1, 1], [1, -1], [1, 1], [-1, -1]]
     expected_output_xor = [1, 1, -1, -1]
 
-    w_min = perceptron(input_data_and,expected_output_and, 0.2, 0, update_weights_simple, module_error_simple, theta_simple, 10000000)
+    w_min = perceptron(input_data_and,expected_output_and, 0.2, 0, update_weights_simple, module_error_simple, theta_simple, 100)
 
     print(w_min)
+
+    input_data = [[-1, 1], [1, -1], [1, 1], [-1, -1]]
+
+    animate_lines(input_data)
+
 
 main()
 
