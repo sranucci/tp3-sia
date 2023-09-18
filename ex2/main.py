cb189157d3dfc4120b4d33b9c69f94f0103aa737
Marcos Gronda <marcosgronda@gmail.com>
2023-09-18 10:51:22 -0300

    feat: ex2 config

--- ex2/main.py
+++ ex2/main.py
@@ -1,3 +1,5 @@
+import random
+
 from perceptron import *
 from linear_perceptron import *
 from non_linear_perceptron import *
@@ -5,20 +7,42 @@ import csv
 
 
 def main():
+    with open("config.json") as file:
+        config = json.load(file)
+
+    if config["seed"] != -1:
+        random.seed(config["seed"])
+
     input_data = []
     output_data = []
-    with open("../training_data/ex2.csv") as file:
+
+    # Obtenemos los valores
+    with open(config["training_data"]) as file:
         reader = csv.reader(file)
         next(reader)        # ignoramos los valores nombres de las columnas
         for row in reader:
             input_data.append(list(map(lambda x: float(x),row[:-1])))       # los datos de input nos los quedamos
             output_data.append(float(row[-1]))
 
-    # min_weights = perceptron(input_data, output_data, 0.5, 1000, update_weights_linear, error_linear, theta_linear,5000000, 1)
+    # Corremos los algoritmos
+    if config["method"]["type"] == "linear":
+        min_weights = perceptron(input_data, output_data, config["learning_constant"], config["epsilon"],
+                                 update_weights_linear, error_linear, theta_linear, config["limit"], 1)
 
-    min_weights_non_linear = perceptron(input_data, output_data, 0.5, 1000, update_weights_linear, error_non_linear, theta_logarithmic,1000000, 0.3, theta_logaritmic_derivative)
+    elif config["method"]["type"] == "non_linear":
+        if config["method"]["theta"] == "tanh":
+            min_weights = perceptron(input_data, output_data, config["learning_constant"], config["epsilon"],
+                                     update_weights_non_linear, error_non_linear, theta_logistic, config["limit"],
+                                     config["method"]["beta"], theta_tanh_derivative)
 
-    print(min_weights_non_linear)
+        elif config["method"]["theta"] == "logistic":
+            min_weights = perceptron(input_data, output_data, config["learning_constant"], config["epsilon"],
+                                     update_weights_non_linear, error_non_linear, theta_tanh, config["limit"],
+                                     config["method"]["beta"], theta_logistic_derivative)
+        else:
+            quit("Invalid theta")
+    else:
+        quit("invalid method type")
 
 
 main()
--- ex2/non_linear_perceptron.py
+++ ex2/non_linear_perceptron.py
@@ -13,16 +13,16 @@ def theta_tanh_derivative(x, beta):
     return beta * (1 - theta_result ** 2)
 
 
-def theta_logarithmic(x, beta):
+def theta_logistic(x, beta):
     return 1 / (1 + math.exp(-2 * x * beta))
 
 
-def theta_logaritmic_derivative(x, beta):
-    theta_result = theta_logarithmic(x, beta)
+def theta_logistic_derivative(x, beta):
+    theta_result = theta_logistic(x, beta)
     return 2 * beta * theta_result * (1 - theta_result)
 
 
-def update_weights_linear(learning_constant, generated_output, data_input, expected_output, weights, theta_derivative, beta):
+def update_weights_non_linear(learning_constant, generated_output, data_input, expected_output, weights, theta_derivative, beta):
     if expected_output != generated_output:
         weights += learning_constant * (expected_output - generated_output) * compute_activation(data_input, weights, theta_derivative, beta) * data_input
 
