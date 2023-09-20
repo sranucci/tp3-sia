65ab2ac11e67328958d664945c6a0ecc7ad678c2
MSGronda <mgronda@itba.edu.ar>
2023-09-20 20:41:49 -0300

    feat: efficient application of activation function
    Co-Authored-By: MarcosCasiraghi <86983868+MarcosCasiraghi@users.noreply.github.com>
    Co-Authored-By: Agustin Z <86991224+azakalik@users.noreply.github.com>
    Co-Authored-By: TobiasPerry <86983120+TobiasPerry@users.noreply.github.com>
    Co-Authored-By: Máximo Rojas Pelliccia <86984301+mrojaspell@users.noreply.github.com>
    Co-Authored-By: Santino Ranucci <86986069+sranucci@users.noreply.github.com>

--- perceptrons/multi_perceptron.py
+++ perceptrons/multi_perceptron.py
@@ -5,6 +5,8 @@ import random
 class NeuronLayer:
     def __init__(self, previous_layer_neuron_amount, current_layer_neurons_amount, activation_function):
         self.activation_function = activation_function
+
+        # Se genera una matrix de (current_layer_neurons_amount x previous_layer_neuron_amount)
         weights = []
         for i in range(current_layer_neurons_amount):
             weights.append([])
@@ -13,26 +15,21 @@ class NeuronLayer:
         self.weights = np.array(weights)
 
     def compute_activation(self, prev_input):
-        result = np.dot(self.weights,prev_input)    # dot product of vectors
-
-        # TODO: ver si se puede hacer mas eficiente
-        output = [self.activation_function(elem) for elem in result]
-
-        return np.array(output)
+        result = np.dot(self.weights, prev_input)   # Producto escalar
+        return self.activation_function(result)     # Se ejecuta la funcion sobre cada elemento del arreglo
 
 
 class MultiPerceptron:
-    def __init__(self, num_entry_neurons, num_hidden_layers, neurons_per_layer, num_output_neurons,
-                 activation_function):
-
+    def __init__(self, num_entry_neurons, num_hidden_layers, neurons_per_layer, num_output_neurons, activation_function):
         self.layers: [NeuronLayer] = []
+
         # Creamos la primera capa
         self.layers.append(NeuronLayer(num_entry_neurons, num_entry_neurons, activation_function))
 
-        # Creamos la primera capa hidden
+        # Creamos la primera capa interna
         self.layers.append(NeuronLayer(num_entry_neurons, neurons_per_layer, activation_function))
 
-        # Creamos el resto de las capas hidden
+        # Creamos el resto de las capas interna
         for i in range(num_hidden_layers - 1):
             self.layers.append(NeuronLayer(neurons_per_layer, neurons_per_layer, activation_function))
 
