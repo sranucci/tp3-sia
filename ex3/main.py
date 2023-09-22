fix: error calculation for multidimensional output
Co-Authored-By: MarcosCasiraghi <86983868+MarcosCasiraghi@users.noreply.github.com>

ex3/main.py
@@ -1,12 +1,9 @@
+import math
 import random
 
 from perceptrons.multi_perceptron import MultiPerceptron
 import ex2.non_linear_perceptron as nlp
 import numpy as np
-
-
-def theta(x):
-    return x
 
 
 # def initialize_weights(layer_amounts):
@@ -57,6 +54,20 @@ def theta(x):
 
 # ----------------------------------------------------
 
+def theta_logistic(x):
+    try:
+        a = math.exp(-2 * x * 1)
+    except OverflowError:
+        a = float("inf")
+    return 1 / (1 + a)
+
+def theta_logistic_derivative(x):
+    theta_result = theta_logistic(x)
+    return 2 * 1 * theta_result * (1 - theta_result)
+
+# TODO: remove!!!!
+random.seed(1)
+
 # Initialize an empty list to store the result
 input = []
 
@@ -83,13 +94,12 @@ with open('../training_data/ej3-digitos.txt', 'r') as file:
 if temp:
     input.append(temp)
 
-expected_output = [1, -1, 1, -1, 1, -1, 1, -1, 1, -1]
+expected_output = [[1, 0], [0, 1], [1, 0], [0, 1], [1, 0], [0, 1], [1, 0], [0, 1], [1, 0], [0, 1]]
 
 epsilon = 0.01
 limit = 100000
 i = 0
-p = MultiPerceptron(35, 2, 5, 1, lambda x: 1 if x >= 0 else -1, lambda x: 1,
-                    0.1)
+p = MultiPerceptron(35, 2, 5, 2, theta_logistic, theta_logistic_derivative, 0.25)
 
 error = None
 min_error = float("inf")
@@ -102,13 +112,13 @@ while min_error > epsilon and i < limit:
     error = p.compute_error(np.array(input), np.array(expected_output))
     if error < min_error:
         min_error = error
-        #
     i += 1
 
 print(min_error, i)
 
-for array in input:
-    print(p.forward_propagation(array))
+
+for array, output in zip(input, expected_output):
+    print(f"expected: {output}, generated: {p.forward_propagation(array)}")
 
 
 

perceptrons/multi_perceptron.py
@@ -14,7 +14,7 @@ class NeuronLayer:
         for i in range(current_layer_neurons_amount):
             weights.append([])
             for j in range(previous_layer_neuron_amount):
-                weights[i].append(random.uniform(-100, 100))
+                weights[i].append(random.uniform(-2, 2))
         self.weights = np.array(weights)
 
     def compute_activation(self, prev_input):
@@ -62,7 +62,6 @@ class MultiPerceptron:
         for idx, layer in enumerate(self.layers):
             layer.weights += delta_w[idx]
 
-
     def compute_error(self, data_input, expected_outputs):
 
         error_vector = []
@@ -71,7 +70,11 @@ class MultiPerceptron:
             output_result = self.forward_propagation(input)
             error_vector.append(np.power(expected_outputs[idx] - output_result, 2))
 
-        return 0.5 * sum(error_vector)
+        total = 0
+        for elem in error_vector:
+            total += sum(elem)
+
+        return 0.5 * total
 
     def back_propagation(self, expected_output, generated_output) -> list:
         delta_w = []